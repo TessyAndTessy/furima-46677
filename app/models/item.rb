f595abfc95faa86911281d9be48857caff032e20
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-02-23 15:51:23 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one    :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_schedule_id, numericality: { other_than: 1, message: "can't be blank" }
 end
