85ba3eba380b3850a5cbc6a43ac99dc6085d4d82
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-02-23 20:41:38 +0900

    itemモデル単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+    name { 'テスト商品' }
+    info { 'テスト説明' }
+    category_id { 2 }
+    status_id { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id { 2 }
+    delivery_schedule_id { 2 }
+    price { 500 }
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('public/images/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
