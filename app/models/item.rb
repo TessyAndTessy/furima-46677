0eb8f0257e78a73a8b9fcf9fc5bd3d1c48143bd4
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-02-23 16:07:01 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,16 @@ class Item < ApplicationRecord
   # has_one    :order
   has_one_attached :image
 
+  validates :name,  presence: true
+  validates :info,  presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
