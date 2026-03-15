889f27724c43002ababdd1be685f4843b3cc7273
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-03-15 21:32:34 +0900

    購入のためのフォーム実装修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
   def index
     @order_address = OrderAddress.new
   end
 
   def create
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
