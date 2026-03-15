6b3e4057687a21b29d71717fc6e4e6e1a1d001f8
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-03-15 19:21:34 +0900

    カラム名設定とバリデーション処理実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id,
+                :city, :address_line, :building, :phone_number, :token
 
   # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'は「3桁ハイフン4桁」の半角文字列の形式で入力してください' }
+    validates :city
+    validates :address_line
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁以上11桁以内の半角数値で入力してください' }
+    validates :token
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     # 各テーブルにデータを保存する処理を書く
