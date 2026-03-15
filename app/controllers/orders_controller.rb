a3b605fc12e745acb78526d5e3cbde138bee7b08
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-03-15 22:37:32 +0900

    テーブルへフォーム情報の保存を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,34 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
+  before_action :move_to_root
 
   def index
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postal_code, :prefecture_id, :city, :address_line, :building, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user == @item.user || @item.order.present?
   end
 end
