5e3dba91c581d067066cb2e854e248d36bbc3a6f
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-03-22 16:16:10 +0900

    フォーム送信でイベント発火実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,11 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById("charge-form");
+  if (!form) return;
+
+  form.addEventListener("submit", (e) => {
+    console.log("フォーム送信時にイベント発火");
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);
