28d83e01ec06e5e4d538a078a992b206fa851759
TessyAndTessy <t.t.c.kimihiko@gmail.com>
2026-03-26 19:14:15 +0900

    削除時動作修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_root, only: [:edit, :update, :destroy]
+  before_action :move_to_root, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
